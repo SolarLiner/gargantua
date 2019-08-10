bd1418eeaec7e573deb1f53fdc23374d48a70e1d
Nathan Graule <solarliner@gmail.com>
2019-08-10 15:52:54 +0200

    Set parametrized benchmarks

--- starless/benches/scene.rs
+++ starless/benches/scene.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate criterion;
 
-use criterion::{black_box, Criterion};
+use criterion::{black_box, Criterion, ParameterizedBenchmark};
 
 use image::{DynamicImage, Pixel, Rgb};
 use nalgebra::{Translation3, Vector3};
@@ -57,36 +57,47 @@ fn setup_scene_gr(max_iter: u32) -> GRScene {
 }
 
 fn crit_bench_flat(c: &mut Criterion) {
-	c.bench_function_over_inputs(
+	c.bench(
 		"scene flat",
-		|b, &(w, h)| {
-			let scn = setup_scene_flat(w, h);
-			b.iter(|| render(scn.clone(), None));
-		},
-		vec![
-			(100, 100),
-			(640, 380),
-			(720, 540),
-			(1920, 1080),
-			(3840, 2160),
-		],
+		ParameterizedBenchmark::new(
+			"scene flat",
+			|b, &(w, h)| {
+				let scn = setup_scene_flat(w, h);
+				b.iter(|| render(scn.clone(), None));
+			},
+			vec![
+				(10, 10),
+				(32, 32),
+				(50, 50),
+				(64, 32),
+				(64, 64),
+				(100, 100),
+				(128, 96),
+				(320, 144),
+			],
+		)
+		.measurement_time(std::time::Duration::from_secs(20)),
 	);
 }
 
 fn crit_bench_gr(c: &mut Criterion) {
-	c.bench_function_over_inputs(
+	c.bench(
 		"scene gr",
-		|b, &iter| {
-			let scn = setup_scene_gr(iter);
-			b.iter(|| render(scn.clone(), None));
-		},
-		vec![10, 30, 50, 100, 300, 500],
+		ParameterizedBenchmark::new(
+			"scene gr",
+			|b, &iter| {
+				let scn = setup_scene_gr(iter);
+				b.iter(|| render(scn.clone(), None));
+			},
+			vec![10, 30, 50, 100, 300, 500],
+		)
+		.measurement_time(std::time::Duration::from_secs(20)),
 	);
 }
 
 fn bench(c: &mut Criterion) {
 	crit_bench_flat(c);
-	// crit_bench_gr(c);
+	crit_bench_gr(c);
 }
 
 criterion_group!(benches, bench);
