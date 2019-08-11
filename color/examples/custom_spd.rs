e8749b445df63fe6b58b75cc68e3dbaf220d4325
Nathan Graule <solarliner@gmail.com>
2019-08-11 23:56:57 +0200

    Formatting

--- color/examples/custom_spd.rs
+++ color/examples/custom_spd.rs
@@ -1,9 +1,16 @@
-use color::{XYZ, spectrum_to_xyz};
+use color::{spectrum_to_xyz, XYZ};
 
 fn main() {
 	let (X, Y, Z) = spectrum_to_xyz(&|y| if y > 560.0 && y < 620.0 { 5.0 } else { 0.0 });
-	let xyz = XYZ {X,Y,Z};
-	println!("Chromaticity/Luminance from spectrum: {:?}", xyz.to_chromaticity());
-	let col = xyz.to_srgb().expect("Couldn't convert to sRGB").normalize().constrain();
-	println!("Color from spectrum: {}\t{:?}", col.to_hex_code(false), col);
+	let xyz = XYZ { X, Y, Z };
+	println!(
+		"Chromaticity/Luminance from spectrum: {:?}",
+		xyz.to_chromaticity()
+	);
+	let col = xyz
+		.to_srgb()
+		.expect("Couldn't convert to sRGB")
+		.normalize()
+		.constrain();
+	println!("Color from spectrum: {}", col.to_hex_code(false));
 }
